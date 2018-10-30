7c9e7a3f6f75f95bf4db177a11a99d15557dbb25
harpreet <harpreet@okhlee.com>
2018-10-30 21:37:12 +0530

    socket basic

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-var http = require('http').Server(express);
-var io = require('socket.io')(http);
+const express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
-let app = express();
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: false}));
-let port = process.env.PORT || 8080;
+//let port = process.env.PORT || 8080;
 
 
+var peopleList = [];
 app.get('/', function (req, res) {
 
     res.send("Server Started in 3000")
@@ -23,27 +24,65 @@ io.on('connection', function (socket) {
 
     socket.on('join', function (data) {
 
+        var peopleObject = {name: data.name, email: data.email};
+        peopleList.push(peopleObject);
+
 
         socket.join(data.email);
 
-        console.log(data.email + "has joined");
+        console.log(JSON.stringify(data) + " has joined");
 
-       // io.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ");
+
+        io.sockets.emit("person_list", peopleList)
+
+        // io.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ");
 
     });
 
-    socket.on('send_message',function (data) {
+    socket.on('send_message', function (data) {
 
-        io.sockets.in(data.email).emit('receieve_message',{msg:data.msg});
+        console.log("This is the data received " + JSON.stringify(data));
+        io.sockets.in(data.email).emit('receieve_message', {msg: data.msg, user: "sender"});
     });
-    socket.on('disconnect', function () {
-        console.log("User is disconnected");
+    socket.on('disconnect', function (data) {
+        console.log("This is data received " + data);
+        console.log(data + " is disconnected");
+
+
+    });
+
+    socket.on('remove_user', function (data) {
+
+        /*for(var i=0;i<peopleList.length;i++){
+
+            if(peopleList[i].email===data){
+                console.log("inside slice");
+                peopleList.slice(i);
+            }
+
+        }*/
+
+        peopleList = peopleList.filter(person => person.email !== data);
+
+        /* console.log("This is the list " + peopleList.length);
+         for (var i = 0; i < peopleList.length; i++) {
+
+             console.log(peopleList[i].name);
+             console.log(peopleList[i].email);
+
+         }*/
+
+
+        io.sockets.emit('updated_list', peopleList);
+        console.log("This is data received " + data);
+        console.log(data + " is disconnected");
+
 
     });
 
 
 });
-app.listen(process.env.PORT || 3000, function () {
+server.listen(process.env.PORT || 3000, function () {
 
     console.log("Server started at port 3000")
 });
